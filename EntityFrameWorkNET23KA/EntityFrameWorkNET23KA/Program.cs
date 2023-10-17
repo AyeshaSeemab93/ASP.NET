60c680026f0160bec6763273ee3d489535b9bbc4
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-10-17 12:31:24 +0300

    Entity FrameWork-database connected successfully

--- EntityFrameWorkNET23KA/EntityFrameWorkNET23KA/Program.cs
+++ EntityFrameWorkNET23KA/EntityFrameWorkNET23KA/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EntityFrameWorkNET23KA.Models;
+using System.Linq; // Import System.Linq for LINQ
+using var db = new TutorialDbContext();
 
+Console.Write("Enter the customer name: ");
+string inputName = Console.ReadLine();
+
+var customer = db.Customers
+    .Where(c => c.Name == inputName)
+    .FirstOrDefault(); // Use FirstOrDefault to handle potential null results(when no such name found)
+
+if (customer != null)
+{
+    Console.WriteLine("Customer Details:");
+    Console.WriteLine("Name: " + customer.Name);
+    Console.WriteLine("Location: " + customer.Location);
+    Console.WriteLine("ID: " + customer.CustomerId);
+    Console.WriteLine("Email: " + customer.Email);
+}
+else
+{
+    Console.WriteLine("Customer not found.");
+}
